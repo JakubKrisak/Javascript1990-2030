a4124944c1702566f6f64e7edd8f47348494fe65
Jakub Krišák <102148634+JakubKrisak@users.noreply.github.com>
2022-05-11 01:25:07 +0200

    Add files via upload

---
+++ myScript2.js
@@ -0,0 +1,35 @@
+for (let i = 1992; i <= 2030; i = i + 4){
+    console.log("P " + i); 
+}
+
+
+console.log("");
+
+
+for (let i = 1990; i <= 2030; i = i + 10){
+    console.log("O " + i); 
+}
+
+
+console.log("");
+
+
+for (let i = 1990; i <= 2030; i = i + 15){
+    console.log("P O " + i);
+}
+
+
+console.log("");
+
+
+let i = 1990;
+while (i <= 2030){
+    i = i + 8;
+    console.log("N " + i);
+    
+    if(i <= 1998);
+        break;
+}
+
+
+
